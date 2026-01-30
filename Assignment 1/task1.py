7deb08a10fd50668b7f0da7faf077dffaaa548a7
harvindersingh2010 <harvindersingh2010@gmail.com>
2026-01-30 13:27:42 +0530

    Initial commit

---
+++ Assignment 1/task1.py
@@ -0,0 +1,8 @@
+input1 = int(input("Enter the first number: "))
+input2 = int(input("Enter the second number: "))
+
+
+print("Addition:", input1+input2)
+print("Subtraction:", input1-input2)
+print("Multiplication:", input1*input2)
+print("Division:", input1/input2)
